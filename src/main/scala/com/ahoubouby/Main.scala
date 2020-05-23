d0631394b9b9fbff7275186fcda2a3c6db905d82
ahoubouby <ahoubouby@gmail.com>
2020-05-23 15:46:50 +0000

    ✨ Add ClusterEventListener
    add actor, init actor and subscribe it to the cluster

--- src/main/scala/com/ahoubouby/Main.scala
+++ src/main/scala/com/ahoubouby/Main.scala
@@ -3,9 +3,14 @@ package com.ahoubouby
 object Main extends App {
 
   import akka.actor.ActorSystem
+  import com.ahoubouby.actors.ClusterDomainEventListener
   import com.typesafe.config.ConfigFactory
 
   val seedConfig = ConfigFactory.load("seed")
   val seedSystem = ActorSystem("words", seedConfig)
+  val eventListenerActor = seedSystem.actorOf(
+    ClusterDomainEventListener.props,
+    ClusterDomainEventListener.name
+  )
 
 }
---
+++ src/main/scala/com/ahoubouby/actors/ClusterDomainEventListener.scala
@@ -0,0 +1,40 @@
+package com.ahoubouby.actors
+
+import akka.actor.{Actor, ActorLogging}
+
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
+import akka.cluster.MemberStatus
+
+class ClusterDomainEventListener extends Actor with ActorLogging {
+
+  import context._
+
+  Cluster(system = system).subscribe(self, classOf[ClusterDomainEvent])
+
+  override def receive: Receive = {
+    case MemberUp(member)     => log.info("member {} is UP", member)
+    case MemberExited(member) => log.info("member {} is EXITED", member)
+    case MemberRemoved(member, previousStatus) =>
+      if (previousStatus == MemberStatus.exiting)
+        log.info("member {}  gracefully exited, REMOVED", member)
+      else
+        log.error("member {} downed after unreachable, REMOVED", member)
+    case UnreachableMember(m)   => log.info("member {} Unreachable", m)
+    case ReachableMember(m)     => log.info("member {} Reachable", m)
+    case s: CurrentClusterState => log.info("current state {}", s)
+  }
+
+  override def postStop(): Unit = {
+    Cluster(system).unsubscribe(self)
+    super.postStop()
+  }
+}
+
+object ClusterDomainEventListener {
+
+  import akka.actor.Props
+
+  def props = Props[ClusterDomainEventListener]
+  def name = "cluster-event-listener"
+}
