55c7b3950a8cb95ddb25a940b4adcef8295591a3
ahoubouby <ahoubouby@gmail.com>
2020-05-25 03:26:50 +0100

    refactor .sh remove host variableé

--- runMaster.sh
+++ runMaster.sh
@@ -2,6 +2,6 @@
 
 portDefault=2554
 hostDefault=127.0.0.1
-echo "running master node at port ${1:-$portDefault} and host ${2:-$hostDefault}"
+echo "running master node at port ${1:-$portDefault} and host $hostDefault"
 set -x
-sbt "run -DPORT=${1:-$portDefault} -DHOST=${2:-$hostDefault}-Dconfig.resource=master.conf"
+sbt "run -DPORT=${1:-$portDefault} -DHOST=$hostDefault -Dconfig.resource=master.conf"
--- runNode1.sh
+++ runNode1.sh
@@ -2,6 +2,6 @@
 
 portDefault=2551
 hostDefault=127.0.0.1
-echo "running seed node at port ${1:-$portDefault} and host ${2:-$hostDefault}"
+echo "running seed node at port ${1:-$portDefault} and host $hostDefault"
 set -x
-sbt "run -DPORT=${1:-$portDefault} -DHOST=${2:-$hostDefault} -Dconfig.resource=seed.conf"
+sbt "run -DPORT=${1:-$portDefault} -DHOST=$hostDefault -Dconfig.resource=seed.conf"
