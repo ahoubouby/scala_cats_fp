a232715082aba59206ac2327287bc0ba06f5b08a
ahoubouby <ahoubouby@gmail.com>
2020-05-29 23:37:19 +0000

    init commit: add functor and impl map function

--- src/main/scala/com/ahoubouby/Main.scala
+++ src/main/scala/com/ahoubouby/Main.scala
@@ -1,12 +1,29 @@
 package com.ahoubouby
 
+import com.ahoubouby.datascture.Functor
+
 object Main extends App {
 
-  import com.ahoubouby.typeclass_.{Json, Person}
-  import com.ahoubouby.typeclass_.Person._
+  val listFunctor = new Functor[List] {
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
 
-  val person = Person(name = "abdelwahed", email = "ahoubouby@gmail.com")
-  val writePersonJson = Json.toJson(person)
-  println(writePersonJson)
+  val optionMap = new Functor[Option] {
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa match {
+      case Some(value) => Some(f(value))
+      case None        => None
+    }
+  }
+
+  val list              = List(1, 3, 4, 4)
+  val listString        = List("abde", "houboub", "hihi", "dd")
+  val option: Option[_] = None
+  // F[A] zip F[B] => F[(A,B)]
+  val zip = list.zip(listString)
+  println(zip)
+  // F[A,B] unzip => (F[A],F[B])
+  println(zip.unzip)
+  println(listFunctor.distribute(zip))
+  println(optionMap.map(option)(identity))
 
 }
