e5ca53143ea53cfd868af61a0eb6d3cd6ce36204
ahoubouby <ahoubouby@gmail.com>
2020-09-08 13:28:28 +0100

    functors: imap

--- src/main/scala/com/ahoubouby/datascture/branching/imap.scala
+++ src/main/scala/com/ahoubouby/datascture/branching/imap.scala
@@ -1,9 +1,10 @@
 package com.ahoubouby.datascture.branching
+
 trait Codec[A] {
+  self =>
   def encode(value: A): String
   def decode(value: String): A
   def imap[B](dec: A => B, enc: B => A): Codec[B] = {
-    val self = this
     new Codec[B] {
       override def encode(value: B): String = self.encode(enc(value))
 
@@ -11,6 +12,7 @@ trait Codec[A] {
     }
   }
 }
+
 object imap extends App {
 
   implicit val doubleCodec: Codec[Double] = new Codec[Double] {
@@ -25,4 +27,9 @@ object imap extends App {
   }
 
   implicit val intCodec: Codec[Int] = stringCodec.imap(_.toInt, _.toString)
+  implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] =
+    c.imap(Box(_), _.value)
+
+  val box: Box[String] = Box("string inside box")
+  println(boxCodec[String].decode("string come from outside"))
 }
